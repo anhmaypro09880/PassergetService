403a48b191131f03f4bc6a8a4484de3e9670da8a
Phan Nguyen Tan Trong <anhmaypro09880@gmail.com>
2022-11-07 12:55:58 +0700

    finished passenger service

---
+++ src/test/java/com/example/passergetservice/PassergetServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.passergetservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassergetServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
